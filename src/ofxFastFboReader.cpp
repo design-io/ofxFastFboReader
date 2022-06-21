9a5f4cd844cb5224bf4c0319e0445657785af6de
NickHardeman <nickhardeman@gmail.com>
2022-06-21 14:53:14 -0400

    update

--- src/ofxFastFboReader.cpp
+++ src/ofxFastFboReader.cpp
@@ -14,7 +14,7 @@ ofxFastFboReader::~ofxFastFboReader()
 	}
 }
 
-bool ofxFastFboReader::readToPixels(ofFbo &fbo, ofPixelsRef pix, ofImageType type)
+bool ofxFastFboReader::readToPixels(ofFbo &fbo, ofPixels& pix, ofImageType type)
 {
 	genPBOs();
 	
@@ -34,7 +34,8 @@ bool ofxFastFboReader::readToPixels(ofFbo &fbo, ofPixelsRef pix, ofImageType typ
 	else if (type == OF_IMAGE_GRAYSCALE)
 	{
 		channels = 1;
-		glType = GL_LUMINANCE;
+//        glType = GL_LUMINANCE;
+        glType = GL_RED;
 	}
 	else
 	{
@@ -83,6 +84,72 @@ bool ofxFastFboReader::readToPixels(ofFbo &fbo, ofPixelsRef pix, ofImageType typ
 	fbo.unbind();
 	
 	return mem != NULL;
+}
+
+bool ofxFastFboReader::readToPixels( ofFbo &fbo, ofFloatPixels& pix, int aGlType ) {
+    genPBOs();
+    
+    int channels = 3;
+    int numBytes = 4;
+    int glType = aGlType;
+    int glReadType = GL_RGB;
+    
+    if( glType == GL_RGB32F ) {
+        // only support RGB32F to float pixels //
+    } else if( glType == GL_RGBA32F ) {
+        glReadType = GL_RGBA;
+        channels = 4;
+    } else if( glType == GL_R32F ) {
+        glReadType = GL_R;
+        channels = 1;
+    } else {
+        // no support for other options right now //
+        return false;
+    }
+    
+    const int width = fbo.getWidth();
+    const int height = fbo.getHeight();
+    
+    if (async)
+    {
+        index = (index + 1) % num_buffers;
+        nextIndex = (index + 1) % num_buffers;
+    }
+    else
+    {
+        index = nextIndex = 0;
+    }
+    
+    size_t nb = width * height * (channels * numBytes);
+    
+    if (nb != num_bytes)
+    {
+        num_bytes = nb;
+        setupPBOs(num_bytes);
+    }
+    
+    glReadBuffer(GL_FRONT);
+    
+    fbo.bind();
+    
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[index]);
+    glReadPixels(0, 0, width, height, glReadType, GL_FLOAT, NULL );// (GLubyte*) NULL + (12) );
+    
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[nextIndex]);
+    float* mem = (float*)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+    
+    if (mem != NULL) {
+        pix.setFromPixels( mem, width, height, channels );
+        glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+    } else {
+        cout << "ofxFastFboReader :: 32F is null " << " | " << ofGetFrameNum() << endl;
+    }
+    
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+    
+    fbo.unbind();
+    
+    return mem != NULL;
 }
 
 void ofxFastFboReader::genPBOs()
--- src/ofxFastFboReader.h
+++ src/ofxFastFboReader.h
@@ -1,15 +1,18 @@
 #pragma once
-
 #include "ofMain.h"
 
-class ofxFastFboReader
-{
+#ifndef OFX_FAST_FBO_READER_INCLUDED
+    #define OFX_FAST_FBO_READER_INCLUDED
+#endif
+
+class ofxFastFboReader {
 public:
 	
 	ofxFastFboReader(const int num_buffers = 3);
 	~ofxFastFboReader();
 	
-	bool readToPixels(ofFbo &fbo, ofPixelsRef pix, ofImageType type = OF_IMAGE_COLOR);
+	bool readToPixels(ofFbo &fbo, ofPixels& pix, ofImageType type = OF_IMAGE_COLOR);
+    bool readToPixels(ofFbo &fbo, ofFloatPixels& pix, int aGlType );
 	
 	bool getAsync() { return async; }
 	void setAsync(bool v) { async = v; }
@@ -20,7 +23,7 @@ protected:
 	
 	GLuint *pboIds;
 	int index, nextIndex;
-	size_t num_bytes;
+	size_t num_bytes = 0;
 	bool async;
 	
 	ofxFastFboReader(const ofxFastFboReader&);
